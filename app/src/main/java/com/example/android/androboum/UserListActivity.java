1c91f66be6be9709ef715f16256c76a85b00efc2
Chilan-Eli YUKSEL <yuksel-eli@hotmail.fr>
2017-12-20 12:23:06 +0100

    commit filter

--- app/src/main/java/com/example/android/androboum/UserListActivity.java
+++ app/src/main/java/com/example/android/androboum/UserListActivity.java
@@ -1,44 +1,94 @@
 package com.example.android.androboum;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserListActivity extends AppCompatActivity {
+    // variable globale pour spécifier si on filtre ou pas
+    boolean filterConnected = false;
+     MyArrayAdapter adapter;
+
     private class MyArrayAdapter extends ArrayAdapter<Profil> {
         List<Profil> liste;
+        FirebaseStorage storage = FirebaseStorage.getInstance();
         private MyArrayAdapter(Context context, int resource, List<Profil> liste) {
             super(context, resource, liste);
             this.liste = liste;
         }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
+/**
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
             TextView tv = new TextView(getContext());
             tv.setText(liste.get(position).getEmail());
             return tv;
         }
+ */
 
-            @Override
-            public int getCount() {
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // on va chercher le bon profil dans la liste
+            Profil p = liste.get(position);
+
+            // on instancie le layout sous la forme d'un objet de type View
+            View layout = View.inflate(getContext(), R.layout.profil_list_item, null);
+
+
+            // on va chercher les trois composants du layout
+            ImageView imageProfilView = (ImageView) layout.findViewById(R.id.imageView);
+            TextView textView = (TextView) layout.findViewById(R.id.textView);
+            ImageView imageConnectedView = (ImageView) layout.findViewById(R.id.imageView2);
+
+            // on télécharge dans le premier composant l'image du profil
+            StorageReference photoRef = storage.getReference().child(p.getEmail() + "/photo.jpg");
+            if (photoRef != null) {
+                Glide.with(getContext()).using(new FirebaseImageLoader())
+                        .load(photoRef)
+                        .skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .placeholder(R.drawable.ic_person_black_24dp)
+                        .into(imageProfilView);
+            }
+
+            // on positionne le email dans le TextView
+            textView.setText(p.getEmail());
+            // si l'utilisateur n'est pas connecté, on rend invisible le troisième composant
+            if (!p.isConnected) {
+                imageConnectedView.setVisibility(View.INVISIBLE);
+            }
+
+            // on retourne le layout
+            return layout;
+        }
+
+        @Override
+        public int getCount() {
             return liste.size();
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         final List<Profil> userList = new ArrayList<>();
@@ -46,7 +96,7 @@ public class UserListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_list);
 
         ListView listeView = (ListView) findViewById(R.id.liste);
-        final MyArrayAdapter adapter = new MyArrayAdapter(this,android.R.layout.simple_list_item_1, userList);
+        adapter = new MyArrayAdapter(this,android.R.layout.simple_list_item_1, userList);
         listeView.setAdapter(adapter);
         listeView.setAdapter(adapter);
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
@@ -66,5 +116,37 @@ public class UserListActivity extends AppCompatActivity {
             }
         };
         mDatabase.addValueEventListener(postListener);
+    }
+
+    private void showFilterDialog(){
+        // on crée un nouvel objet de type boite de dialogue
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // on lui affecte un titre, et une liste de choix possibles
+        builder.setTitle(R.string.filter_dialog_title)
+                .setSingleChoiceItems(R.array.users_filter, filterConnected?0:1, new DialogInterface.OnClickListener() {
+                    @Override
+                    // méthode appelée quand l'utilisateur fait un choix
+                    // i contient le numéro du choix
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // si le premier item a été choisie, on filtre sur
+                        // uniquement les utilisateurs connectés.
+                        filterConnected = (i == 0);
+                        // et on signale a l'adaptateur qu'il faut remettre
+                        // la liste à jour.
+                        adapter.notifyDataSetChanged();
+                    }
+                })
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    // on a cliqué sur "ok", on ne fait rien.
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+
+        // on crée la boite
+        AlertDialog dialog = builder.create();
+        // et on l'affiche
+        dialog.show();
     }
 }
